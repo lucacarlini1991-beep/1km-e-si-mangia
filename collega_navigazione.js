1824b7394033764c138b1ab7f4ce8aa3520dbc49
lucacarlini1991-beep <lucacarlini1991@gmail.com>
2026-09-24 21:48:17 +0200

    Rimuovi file legacy non utilizzato: collega_navigazione.js

--- collega_navigazione.js
+++
@@ -1,9 +0,0 @@
-// 1 KM E SI MANGIA
-// COLLEGA_NAVIGAZIONE
-//
-// Il pulsante NAVIGA è gestito direttamente da script.js.
-// Questo file non deve più iniettare pulsanti nelle schede.
-
-console.log(
-  "COLLEGA_NAVIGAZIONE.JS: nessuna iniezione NAVIGA."
-);
